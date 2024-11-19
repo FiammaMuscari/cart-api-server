9fc4d8e51aa56ff8ae9fb6715e1851dd6c897b36
Fiammamuscari <fiammamuscari@gmail.com>
2024-11-18 21:14:50 -0300

    📌 feat: cart api server on render

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cartRoutes from "./routes.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://wallbit-challenge-vert.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/api/cart", cartRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
